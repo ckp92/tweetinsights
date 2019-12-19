f9bd55610392369b4cfa4c0063e67b190d656ae4
Chintan Patel <chintan_patel818@hotmail.co.uk>
2019-12-19 10:09:57 +0000

    add react-router-dom an setup basic route structure. refactor components and styles to match

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,29 @@
 import "../styles/App.css";
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+const Header = () => <h2>Header!</h2>;
+const Docs = () => <h2>Docs!</h2>;
+const CV = () => <h2>CV!</h2>;
+const GitHub = () => <h2>GitHub!</h2>;
+const Contact = () => <h2>Contact!</h2>;
+const Main = () => <h2>Main!</h2>;
 
 const App = () => {
-  return <div className="app">App!</div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <div className="container">
+          <Header />
+          <Route path="/" exact component={Main} />
+          <Route path="/docs" exact component={Docs} />
+          <Route path="/cv" exact component={CV} />
+          <Route path="/github" exact component={GitHub} />
+          <Route path="/contact" exact component={Contact} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
